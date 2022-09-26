17a25e4baf0c43ffcd52598a31a1f708dcb2c38f
phyeonguk cheon <phyeongkr@gmail.com>
2022-09-26 10:12:09 +0900

    2022.09.26 10:12

--- PythonProgramming/12_Knapsack_GraphOptimizationProblems.py
+++ PythonProgramming/12_Knapsack_GraphOptimizationProblems.py
@@ -166,3 +166,78 @@ class Graph(Digraph):
         rev = Edge(edge.getDestination(), edge.getSource())
         Digraph.addEdge(self, rev)
 
+def printPath(path):
+    """ Assumes path is a list of nodes """
+    result = ''
+    for i in range(len(path)):
+        result = result + str(path[i])
+        if i != len(path) - 1:
+            result = result + '->'
+    return result
+
+def DFS(graph, start, end, path, shortest, toPrint = False):
+    """ Assumes graph is a Digraph; start and end are nodes;
+                path and shortest are list of nodes
+        Returns a shortest path from start to end in graph """
+    path = path + [start]
+    if toPrint:
+        print('Current DFS path:', printPath(path))
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path:    # avoid cycles
+            if shortest == None or len(path) < len(shortest):
+                newPath = DFS(graph, node, end, path, shortest, toPrint)
+            if newPath != None:
+                shortest = newPath
+    return shortest
+
+def shortestPath(graph, start, end, toPrint = False):
+    """ Assumes graph is a Digraph; start to end are nodes
+        Returns a shortest path from start to end in graph """
+    return DFS(graph, start, end, [], None, toPrint)
+
+def BFS(graph, start, end, toPrint = False):
+    """ Assumes graph is a Digraph; start and end are nodes
+        Returns a shortest path form start to end in graph """
+    initPath = [start]
+    pathQueue = [initPath]
+    # if toPrint:
+    #     print('Current BFS path:', printPath(path))
+    while len(pathQueue) != 0:
+        tmpPath = pathQueue.pop(0)
+        if toPrint:
+            print('Current BFS path:', printPath(tmpPath))
+        lastNode = tmpPath[-1]
+        if lastNode == end:
+            return tmpPath
+        for nextNode in graph.childrenOf(lastNode):
+            if nextNode not in tmpPath:
+                newPath = tmpPath + [nextNode]
+                pathQueue.append(newPath)
+    return None
+
+def testSP():
+    nodes = []
+    for name in range(6):   # create 6 nodes
+        nodes.append(Node(str(name)))
+    g = Digraph()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(Edge(nodes[0], nodes[1]))
+    g.addEdge(Edge(nodes[1], nodes[2]))
+    g.addEdge(Edge(nodes[2], nodes[3]))
+    g.addEdge(Edge(nodes[2], nodes[4]))
+    g.addEdge(Edge(nodes[3], nodes[4]))
+    g.addEdge(Edge(nodes[3], nodes[5]))
+    g.addEdge(Edge(nodes[0], nodes[2]))
+    g.addEdge(Edge(nodes[1], nodes[0]))
+    g.addEdge(Edge(nodes[3], nodes[1]))
+    g.addEdge(Edge(nodes[4], nodes[0]))
+    sp = shortestPath(g, nodes[0], nodes[5], toPrint = True)
+    print('Shortest path found by DFS:', printPath(sp))
+    sp = BFS(g, nodes[0], nodes[5], toPrint = True)
+    print('Shortest path found by BFS:', printPath(sp))
+    
+testSP()
+
