0cad15bc1282880362ca41f73a7c93eb08961bb2
phyeonguk cheon <phyeongkr@gmail.com>
2022-09-28 09:31:12 +0900

    2022.09.28 09:30

--- PythonProgramming/13_Dynamic_Programming.py
+++ PythonProgramming/13_Dynamic_Programming.py
@@ -1,27 +1,27 @@
-# def Fib(n):
-#     """ Assumes n is an int >= 0
-#         Returns Fibonacci of n """
-#     if n == 0 or n == 1:
-#         return 1
-#     result = Fib(n-1) + Fib(n-2)
-#     return result
-# def fastFib(n, memo = {}):
-#     """ Assumes n is an int >= 0; memo used only by recursive calls
-#         Returns Fibonacci of n """
-#     if memo == None:
-#         memo = {}
-#     if n == 0 or n == 1:
-#         return 1
-#     try:
-#         return memo[n]
-#     except KeyError:
-#         result = fastFib(n-1, memo) + fastFib(n-2, memo)
-#         memo[n] = result
-#         return result
-# def testFib(n):
-#     print(fastFib(n))
-#     print(Fib(n))
-# # testFib(35)
+def Fib(n):
+    """ Assumes n is an int >= 0
+        Returns Fibonacci of n """
+    if n == 0 or n == 1:
+        return 1
+    result = Fib(n-1) + Fib(n-2)
+    return result
+def fastFib(n, memo = {}):
+    """ Assumes n is an int >= 0; memo used only by recursive calls
+        Returns Fibonacci of n """
+    if memo == None:
+        memo = {}
+    if n == 0 or n == 1:
+        return 1
+    try:
+        return memo[n]
+    except KeyError:
+        result = fastFib(n-1, memo) + fastFib(n-2, memo)
+        memo[n] = result
+        return result
+def testFib(n):
+    print(fastFib(n))
+    print(Fib(n))
+# testFib(35)
 
 class Item(object):
     def __init__(self, n, v, w):
@@ -37,12 +37,6 @@ class Item(object):
     def __str__(self):
         result = '<' + self.name + ', ' + str(self.value) + ', ' + str(self.weight) + '>'
         return result
-# def value(item):
-#     return item.getValue()
-# def weightInverse(item):
-#     return 1.0 / item.getWeight()
-# def density(item):
-#     return item.getValue() / item.getWeight()
 
 def maxVal(toConsider, avail):
     """ Assumes toConsider a list of items, avail a weight
@@ -62,7 +56,6 @@ def maxVal(toConsider, avail):
         else:
             result = (withoutVal, withoutToTake)
     return result
-
 def smallTest():
     names = ['a', 'b', 'c', 'd']
     vals = [6, 7, 8, 9]
@@ -74,4 +67,53 @@ def smallTest():
     for item in taken:
         print(item)
     print('Total value of items taken =', val)
-smallTest()
+# smallTest()
+import random
+def buildManyItems(numItems, maxVal, maxWeight):
+    items = []
+    for i in range(numItems):
+        items.append(Item(str(i),
+                          random.randint(1, maxVal),
+                          random.randint(1, maxWeight)))
+    return items
+def bigTest(numItems):
+    items = buildManyItems(numItems, 10, 10)
+    val, taken = maxVal(items, 40)
+    print('Items Taken')
+    for item in taken:
+        print(item)
+    print('Total value of items taken =', val)
+# bigTest(30)
+
+def fastMaxVal(toConsider, avail, memo = {}):
+    """ Assumes toConsider a list of items, avail a weight,
+            memo supplied by recursive calls
+        Returns a tuple of the total value of a solution
+            to the 0/1 knapsack problem and the items of that solution """
+    if memo == None:
+        memo = {}
+    if (len(toConsider), avail) in memo:
+        return memo[(len(toConsider), avail)]
+    elif toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getWeight() > avail:
+        result = fastMaxVal(toConsider[1:], avail, memo)
+    else:
+        nextItem = toConsider[0]
+        withVal, withToTake = fastMaxVal(toConsider[1:], avail - nextItem.getWeight(), memo)
+        withVal += nextItem.getValue()
+        withoutVal, withoutToTake = fastMaxVal(toConsider[1:], avail, memo)
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    memo[(len(toConsider), avail)] = result
+    return result
+def bigTest2(numItems):
+    items = buildManyItems(numItems, 10, 10)
+    val, taken = fastMaxVal(items, 40)
+    print('Items Taken')
+    for item in taken:
+        print(item)
+    print('Total value of items taken =', val)
+bigTest2(40)
