5e21b9637761131a55bf7e6d09773c86fc69b79b
phyeonguk cheon <phyeongkr@gmail.com>
2022-10-28 16:49:38 +0900

    #

--- PythonProgramming/15_StochasticPrograms_Probability_Distributions.py
+++ PythonProgramming/15_StochasticPrograms_Probability_Distributions.py
@@ -41,3 +41,35 @@ def regressToMean(numFlips, numTrials):
     pylab.legend(loc = 'best')
     pylab.show()
 # regressToMean(15, 40)
+
+def flipPlot(minExp, maxExp):
+    """ Assumes minExp and maxExp positive integers; minExp < maxExp 
+        Plots results of 2**minExp to 2**maxExp coin flips """
+    ratios, diffs, xAxis = [], [], []
+    for exp in range(minExp, maxExp+1):
+        xAxis.append(2**exp)
+    for numFlips in xAxis:
+        numHeads = 0
+        for n in range(numFlips):
+            if random.choice(('H', 'T')) == 'H':
+                numHeads += 1
+        numTails = numFlips - numHeads
+        try:
+            ratios.append(numHeads/numTails)
+            diffs.append(abs(numHeads - numTails))
+        except ZeroDivisionError:
+            continue
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails)')
+    pylab.plot(xAxis, diffs, 'k')
+#    pylab.plot(xAxis, diffs, 'ko')
+    pylab.figure()
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('#Heads/#Tails')
+    pylab.plot(xAxis, ratios, 'k')
+#    pylab.plot(xAxis, ratios, 'ko')
+    pylab.show()
+# random.seed(0)
+# flipPlot(4, 20)
